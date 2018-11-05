25084bc981d124ce8dca8a873221ebc51d8c03df
Manuel García González <manuel.garcia@erycia.com>
2018-11-05 09:39:31 -0600

    ADD:  - ConnectionInformix

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using ConsoleApp.Data;
+using System;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            var connectionInformix = new ConnectionInformix();
+            Console.WriteLine(connectionInformix.Test());
+
+            var data = connectionInformix.GetData("SELECT * FROM user_web");
+            Console.WriteLine($"\nRows: {data.Rows.Count}");
+
+            Console.ReadKey();
+        }
+    }
+}
